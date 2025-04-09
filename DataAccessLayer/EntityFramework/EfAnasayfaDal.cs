0f27b036f06fbd165259efa1d908afadca565f39
Esra Derici <esraderici@outlook.com.tr>
2025-04-09 21:50:07 +0300

    task3

---
+++ DataAccessLayer/EntityFramework/EfAnasayfaDal.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.Repositories;
+using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfAnasayfaDal : GenericRepository<Anasayfa>, IAnasayfaDal
+    {
+        public EfAnasayfaDal(Context context) : base(context)
+        {
+        }
+    }
+}
